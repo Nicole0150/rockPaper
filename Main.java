a2b81de972be3b41a1b2991bc73a8d0274b9bb7e
Nicole0150 <nicole.williamson@barlcays.com>
2024-11-08 13:59:21 +0000

    refactored nested if statements to improve readability

--- Main.java
+++ Main.java
@@ -19,42 +19,26 @@ public class Main {
             System.out.println("Opponent chose: " + opponentChoice);
         
 
-            if (myChoice.equals("rock")) {
-                if (opponentChoice.equals("rock")) {
-                    System.out.println("It's a tie");
-                } else if (opponentChoice.equals("paper")) {
-                    System.out.println("Opponent wins");
-                    opponentScore++;
-                } else {
-                    System.out.println("You win");
-                    playerScore++;
-                }
-                System.out.println("Player: " + playerScore + " Opponent: " + opponentScore);
-            } else if (myChoice.equals("paper")) {
-                if (opponentChoice.equals("paper")) {
-                    System.out.println("It's a tie");
-                } else if (opponentChoice.equals("scissors")) {
-                    System.out.println("Opponent wins");
-                    opponentScore++;
-                } else {
-                    System.out.println("You win");
-                    playerScore++;
-                }
-                System.out.println("Player: " + playerScore + " Opponent: " + opponentScore);
-            } else if (myChoice.equals("scissors")) {
-                if (opponentChoice.equals("scissors")) {
-                    System.out.println("It's a tie");
-                } else if (opponentChoice.equals("rock")) {
-                    System.out.println("Opponent wins");
-                    opponentScore++;
-                } else {
-                    System.out.println("You win");
-                    playerScore++;
-                }
-                System.out.println("Player: " + playerScore + " Opponent: " + opponentScore);
+            if (myChoice.equals(opponentChoice)){
+                System.out.println("It's a tie!");
+            } else if (
+                (myChoice.equals("rock") && opponentChoice.equals("scissors")) || 
+                (myChoice.equals("paper") && opponentChoice.equals("rock")) ||
+                (myChoice.equals("scissors") && opponentChoice.equals("paper"))
+            ){
+                System.out.println("You win!");
+                playerScore++;
+            }else if (
+                (myChoice.equals("scissors") && opponentChoice.equals("rock")) || 
+                (myChoice.equals("rock") && opponentChoice.equals("paper")) ||
+                (myChoice.equals("paper") && opponentChoice.equals("scissors"))
+            ){
+                System.out.println("Opponent Wins");
+                opponentScore++;
             } else {
                 System.err.println("Invalid input! Please choose rock, paper, or scissors.");
             }
+            System.out.println("Player score: " + playerScore + " Opponent Score: " + opponentScore);
 
             if(playerScore + opponentScore == 3){
                 break;
@@ -68,3 +52,37 @@ public class Main {
         }
     }
 }
+
+            // if (myChoice.equals("rock")) {
+            //     if (opponentChoice.equals("rock")) {
+            //         System.out.println("It's a tie");
+            //     } else if (opponentChoice.equals("paper")) {
+            //         System.out.println("Opponent wins");
+            //         opponentScore++;
+            //     } else {
+            //         System.out.println("You win");
+            //         playerScore++;
+            //     }
+            //     System.out.println("Player: " + playerScore + " Opponent: " + opponentScore);
+            // } else if (myChoice.equals("paper")) {
+            //     if (opponentChoice.equals("paper")) {
+            //         System.out.println("It's a tie");
+            //     } else if (opponentChoice.equals("scissors")) {
+            //         System.out.println("Opponent wins");
+            //         opponentScore++;
+            //     } else {
+            //         System.out.println("You win");
+            //         playerScore++;
+            //     }
+            //     System.out.println("Player: " + playerScore + " Opponent: " + opponentScore);
+            // } else if (myChoice.equals("scissors")) {
+            //     if (opponentChoice.equals("scissors")) {
+            //         System.out.println("It's a tie");
+            //     } else if (opponentChoice.equals("rock")) {
+            //         System.out.println("Opponent wins");
+            //         opponentScore++;
+            //     } else {
+            //         System.out.println("You win");
+            //         playerScore++;
+            //     }
+            //     System.out.println("Player: " + playerScore + " Opponent: " + opponentScore);
