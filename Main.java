3cbdf25b0257a1c01063ed87d80594f8d391a9d5
Nicole0150 <nicole.williamson@barlcays.com>
2024-11-08 13:37:46 +0000

    another v of the game where opponent can choose their move

--- Main.java
+++ Main.java
@@ -2,28 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("WELCOME TO ROCK, PAPER, SCISSORS.  BEST OF 3");
         Scanner scanner = new Scanner(System.in);
         int playerScore = 0;
         int opponentScore = 0;
         while(true){
             System.out.println("Rock, paper, scissors? Enter Quit to quit the game.");
             String myChoice = scanner.nextLine().trim().toLowerCase();
+            System.out.println("Opponent choose: Rock, Paper, Scissors? (Quit will exit game)");
+            String opponentChoice = scanner.nextLine().trim().toLowerCase();
 
-            if(myChoice.equals("quit")){
+            if(myChoice.equals("quit")|| opponentChoice.equals("quit")){
                 break;
             } 
-
-            int opponentNum = (int) (Math.random() * 3); 
-            String opponentChoice;
-
-            if (opponentNum == 0) {
-                opponentChoice = "rock";
-            } else if (opponentNum == 1) {
-                opponentChoice = "paper";
-            } else {
-                opponentChoice = "scissors";
-            }
-
+            System.out.println("You chose: " + myChoice);
             System.out.println("Opponent chose: " + opponentChoice);
         
 
@@ -63,7 +55,16 @@ public class Main {
             } else {
                 System.err.println("Invalid input! Please choose rock, paper, or scissors.");
             }
+
+            if(playerScore + opponentScore == 3){
+                break;
+            }
         }
         System.out.println("Thank you for playing!");
+        if(playerScore>opponentScore){
+            System.out.println("You win!");
+        } else if(opponentScore>playerScore){
+            System.out.println("Opponent wins!");
+        }
     }
 }
