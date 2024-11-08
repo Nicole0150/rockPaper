166150a4d0a6e179f9514ecc001342ecd82e6654
Nicole0150 <nicole.williamson@barlcays.com>
2024-11-08 13:24:34 +0000

    Score functionality implemented

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int playerScore = 0;
+        int opponentScore = 0;
         while(true){
             System.out.println("Rock, paper, scissors? Enter Quit to quit the game.");
             String myChoice = scanner.nextLine().trim().toLowerCase();
@@ -23,34 +25,45 @@ public class Main {
             }
 
             System.out.println("Opponent chose: " + opponentChoice);
+        
 
             if (myChoice.equals("rock")) {
                 if (opponentChoice.equals("rock")) {
                     System.out.println("It's a tie");
                 } else if (opponentChoice.equals("paper")) {
                     System.out.println("Opponent wins");
+                    opponentScore++;
                 } else {
                     System.out.println("You win");
+                    playerScore++;
                 }
+                System.out.println("Player: " + playerScore + " Opponent: " + opponentScore);
             } else if (myChoice.equals("paper")) {
                 if (opponentChoice.equals("paper")) {
                     System.out.println("It's a tie");
                 } else if (opponentChoice.equals("scissors")) {
                     System.out.println("Opponent wins");
+                    opponentScore++;
                 } else {
                     System.out.println("You win");
+                    playerScore++;
                 }
+                System.out.println("Player: " + playerScore + " Opponent: " + opponentScore);
             } else if (myChoice.equals("scissors")) {
                 if (opponentChoice.equals("scissors")) {
                     System.out.println("It's a tie");
                 } else if (opponentChoice.equals("rock")) {
                     System.out.println("Opponent wins");
+                    opponentScore++;
                 } else {
                     System.out.println("You win");
+                    playerScore++;
                 }
+                System.out.println("Player: " + playerScore + " Opponent: " + opponentScore);
             } else {
                 System.err.println("Invalid input! Please choose rock, paper, or scissors.");
             }
         }
+        System.out.println("Thank you for playing!");
     }
 }
